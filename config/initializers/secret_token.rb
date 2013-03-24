e391f59e8c0fa0c728de157884567a2621f67717
Takuto Nishioka <ntakuto@gmail.com>
2013-03-24 20:24:48 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChefRailsTemplate::Application.config.secret_token = 'b96f2303ebb734aefd68c305083ec749f4b6a49b7836f6734165d0f5be2e1c339f55a3e4655ce29149328886f090d9503db26156fd103bc28632ddb9b96e6000'
